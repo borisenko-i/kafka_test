73d6abaada1e96ad6e9e5f510505bb5d31c17c85
ifursova <irina.fursova@fivetran.com>
2022-06-06 11:54:00 +0300

    Javadoc removed.

--- web_app/src/main/java/com/kafka_test/web_app/resources/TestResource.java
+++ web_app/src/main/java/com/kafka_test/web_app/resources/TestResource.java
@@ -7,19 +7,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-/**
- * Root resource (exposed at "myresource" path)
- */
 @Path("test_resource")
 public class TestResource {
     private static List<Resource> resources = new ArrayList<>();
 
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String get() {
