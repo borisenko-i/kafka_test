1f082693f45f44f59b72a6fdf31680aa1c094faa
ifursova <irina.fursova@fivetran.com>
2022-06-06 11:52:59 +0300

    Test POST API.

---
+++ web_app/src/main/java/com/kafka_test/web_app/resources/Resource.java
@@ -0,0 +1,5 @@
+package com.kafka_test.web_app.resources;
+
+public class Resource {
+    public String resourceName;
+}
--- web_app/src/main/java/com/kafka_test/web_app/resources/TestResource.java
+++ web_app/src/main/java/com/kafka_test/web_app/resources/TestResource.java
@@ -1,15 +1,18 @@
 package com.kafka_test.web_app.resources;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Root resource (exposed at "myresource" path)
  */
 @Path("test_resource")
 public class TestResource {
+    private static List<Resource> resources = new ArrayList<>();
 
     /**
      * Method handling HTTP GET requests. The returned object will be sent
@@ -20,6 +23,15 @@ public class TestResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String get() {
-        return "Test Resource!";
+        Resource defaultResource = new Resource();
+        defaultResource.resourceName = "Test Resource Default";
+        if (resources.isEmpty()) resources.add(defaultResource);
+        return resources.stream().map(x -> x.resourceName).collect(Collectors.joining("\n"));
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void create(Resource resource) {
+        resources.add(resource);
     }
 }
